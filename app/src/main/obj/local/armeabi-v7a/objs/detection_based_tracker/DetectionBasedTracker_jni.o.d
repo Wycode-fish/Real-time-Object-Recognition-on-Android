18b6ca48aec74f389117600c0bf521f055c7e0e1
eason <164335424@qq.com>
2017-08-23 11:12:10 +0800

    first commit

---
+++ app/src/main/obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,50 @@
+/home/eason/workspace/android_project/FaceDetect/app/src/main/obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
+  /home/eason/workspace/android_project/FaceDetect/app/src/main/jni/DetectionBasedTracker_jni.cpp \
+  /home/eason/workspace/android_project/FaceDetect/app/src/main/jni/DetectionBasedTracker_jni.h \
+  /home/eason/Softwares/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp \
+  /home/eason/Softwares/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+  /home/eason/Softwares/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+  /home/eason/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm \
+  /home/eason/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cmath \
+  /home/eason/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef \
+  /home/eason/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/complex \
+  /home/eason/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/map \
+  /home/eason/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/vector \
+  /home/eason/Softwares/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+  /home/eason/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/limits \
+  /home/eason/Softwares/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+  /home/eason/Softwares/OpenCV-android-sdk/sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp \
+  /home/eason/Softwares/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect.hpp \
+  /home/eason/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/deque
+
+/home/eason/workspace/android_project/FaceDetect/app/src/main/jni/DetectionBasedTracker_jni.h:
+
+/home/eason/Softwares/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp:
+
+/home/eason/Softwares/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+/home/eason/Softwares/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+
+/home/eason/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm:
+
+/home/eason/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cmath:
+
+/home/eason/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef:
+
+/home/eason/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/complex:
+
+/home/eason/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/map:
+
+/home/eason/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/vector:
+
+/home/eason/Softwares/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/home/eason/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/limits:
+
+/home/eason/Softwares/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+
+/home/eason/Softwares/OpenCV-android-sdk/sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp:
+
+/home/eason/Softwares/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect.hpp:
+
+/home/eason/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/deque:
